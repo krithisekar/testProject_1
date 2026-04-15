1344e2a65df2e5059cc575a69bc339ba845e49f2
krithisekar <58632168+krithisekar@users.noreply.github.com>
2026-04-15 17:37:21 +0530

    Change header text to 'Manage test Users'

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ include 'backend/database.php';
             <div class="table-title">
                 <div class="row">
                     <div class="col-sm-6">
-						<h2>Manage <b>Users</b></h2>
+						<h2>Manage test <b>Users</b></h2>
 					</div>
 					<div class="col-sm-6">
 						<a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New User</span></a>
